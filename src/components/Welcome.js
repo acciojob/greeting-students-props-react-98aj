54223ad5d9abdd5cc20fbb8f20d7f7f91eb5033e
98aj <103376356+98aj@users.noreply.github.com>
2023-03-17 22:39:18 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,7 @@ export default function Welcome(prop) {
   return (
     <div>
       <h1>Hey {prop.name}!</h1>
-      <h2>Welcome to School.</h2>
+      <h2>Welcome to school.</h2>
     </div>
   )
 }
