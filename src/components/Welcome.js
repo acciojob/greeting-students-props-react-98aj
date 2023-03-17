6213422c57ea4da08aca2104576923d3fd13a8c5
98aj <103376356+98aj@users.noreply.github.com>
2023-03-17 22:21:14 +0530

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,1 +1,12 @@
 // write code for Welcome component here
+import React from 'react'
+
+export default function welcome(prop){
+      return(
+        <div>
+            <h1>Hey {prop.name}!</h1>  
+            <h2>Welcome to school.</h2>
+        </div>
+      )
+    
+}
